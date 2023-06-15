48f873fe89d2ffa17879be93cccdff8a0a393959
splendor2023 <uchechukwu735@gmail.com>
2023-06-14 17:10:08 -0700

    changes made on the previous code now correct

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,38 +1,41 @@
 #!/usr/bin/python3
 
 def to_subtract(list_num):
-    subtract_total = 0
-    max_num = max(list_num)
+    to_sub = 0
+    max_list = max(list_num)
 
-    for num in list_num:
-        if max_num > num:
-            subtract_total += num
+    for k in list_num:
+        if max_list > k:
+            to_sub += k
 
-    return (max_num - subtract_total)
+    return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
-        return (0)
+    if not roman_string:
+        return 0
+
+    if not isinstance(roman_string, str):
+        return 0
 
     rom_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_digits = list(roman_nums.keys())
+    list_keys = list(rom_n.keys())
 
-    total = 0
-    last_roman_num = 0
-    num_list = [0]
+    num = 0
+    last_rom = 0
+    list_num = [0]
 
     for char in roman_string:
-        for roman_num in roman_digits:
-            if roman_num == char:
-                if rom_num.get(char) <= last_roman_num:
-                    total += to_subtract(num_list)
-                    num_list = [rom_num.get(char)]
+        for r_num in list_keys:
+            if r_num == char:
+                if rom_num.get(char) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_num.get(char)]
                 else:
-                    num_list.append(rom_num.get(char))
+                    list_num.append(rom_num.get(char))
 
-                last_roman_num = rom_num.get(char)
+                last_rom = rom_num.get(char)
 
-    total += to_subtract(num_list)
+    num += to_subtract(list_num)
 
-    return (total)
+    return (num)
